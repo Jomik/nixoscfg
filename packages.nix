3a58ae1493257c15ffc981893754b0ef8965204b
Jonas <Jomik@users.noreply.github.com>
2015-10-27 17:39:39 +0100

    Update packages.nix

--- packages.nix
+++ packages.nix
@@ -13,7 +13,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    wget curl gitAndTools.gitFull chromium vim emacs
+    wget curl gitAndTools.gitFull chromium vim
   ];
 
   programs.zsh.enable = true;
