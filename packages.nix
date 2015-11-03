f53ba21b7bb3e99e1624248bc0ec221166a6c6d0
Jonas Damtoft <Jomik@users.noreply.github.com>
2015-11-03 14:45:52 +0100

    Add emacs

--- packages.nix
+++ packages.nix
@@ -13,7 +13,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    wget curl gitAndTools.gitFull chromium vim kde5.kwalletmanager
+    wget curl gitAndTools.gitFull chromium vim emacs
   ];
   services.locate.enable = true;
 }
