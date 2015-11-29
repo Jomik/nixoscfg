b586e72d15bbe5930b5fd3fb5f89dd8611f53893
Jonas Damtoft <Jomik@noreply.users.github.com>
2015-11-29 17:56:24 +0100

    Remove excessive fonts

--- packages.nix
+++ packages.nix
@@ -38,25 +38,9 @@
     enableGhostscriptFonts = true;
     enableCoreFonts = true;
     fonts = with pkgs; [
-      clearlyU
-      cm_unicode
-      dejavu_fonts
-      dosemu_fonts
-      font-awesome-ttf
-      freefont_ttf
       inconsolata
       powerline-fonts
-      proggyfonts
       source-code-pro
-      source-sans-pro
-      source-serif-pro
-      terminus_font
-      tewi-font
-      ttf_bitstream_vera
-      ubuntu_font_family
-      unifont
-      vistafonts
-      wqy_microhei
       hack-font
     ] ++ lib.filter lib.isDerivation (lib.attrValues lohit-fonts);
   };
